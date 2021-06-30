abb0ff31815dd443cd7aace51ad2d79c8401d1de
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-06-30 19:47:43 +0530

    java oop practice problem

---
+++ JavaProgram/OOPConcept/Circle.java
@@ -0,0 +1,27 @@
+package JavaProgram.OOPConcept;
+
+class circle1{
+    
+    public double radius;
+    
+    public double area(){
+        return Math.PI*radius*radius;
+    }
+
+    public double circumference(){
+        return 2*(Math.PI)*radius;
+    }
+}
+
+
+public class Circle {
+    public static void main(String[] args) {
+        
+        circle1 c = new circle1();
+        c.radius = 10.5;
+
+        System.out.printf("%.2f", c.area());
+        System.out.println();
+        System.out.printf("%.2f", c.circumference());
+    }
+}
---
+++ JavaProgram/OOPConcept/ClassAndObject.java
@@ -0,0 +1,31 @@
+package JavaProgram.OOPConcept;
+
+class rectangle{
+    
+    public int length;
+    public int breadth;
+
+    public int area(){
+        return length*breadth;
+    }
+
+    public int perimeter(){
+        return 2*(length+breadth);
+    }
+
+}
+
+public class ClassAndObject{
+
+    public static void main(String[] args) {
+
+        rectangle r = new rectangle();
+
+        r.length = 10;
+        r.breadth = 20;
+
+        System.out.println(r.area());
+        System.out.println(r.perimeter());
+        
+    }
+}
