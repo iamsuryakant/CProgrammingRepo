358aa0f61b98d1e813b1dc7df2c1ec61cbe6d93b
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-06-30 20:52:04 +0530

    java oop practice problem

---
+++ JavaProgram/OOPConcept/Cylinder.java
@@ -0,0 +1,70 @@
+package JavaProgram.OOPConcept;
+
+class Cylinder1{
+
+    private double radius;
+    private double height;
+
+    double getRadius(){
+        return radius;
+    }
+
+    double getHeight(){
+        return height;
+    }
+
+    void setRadius(double r){
+
+        if(r>0)
+        {
+            radius = r;
+        }
+        else{
+            radius = 0;
+        }
+    }
+
+    void setHeight(double h){
+        if(h>0)
+        {
+            height = h;
+        }
+        else{
+            height = 0;
+        }
+    }
+
+    public double volume(){
+        return Math.PI*radius*radius*height;
+    }
+
+    public double totalSurfaceA()
+    {
+        return (2*Math.PI*radius)*(radius+height);
+    }
+
+    public Cylinder1()
+    {
+        radius = 1;
+        height = 1;
+    }
+
+    public Cylinder1(double r, double h)
+    {
+        radius = r;
+        height = h;
+    }
+
+}
+
+public class Cylinder {
+
+    public static void main(String[] args) {
+        
+        Cylinder1 c1 = new Cylinder1(10,20);
+
+        System.out.println(c1.volume());
+        System.out.println(c1.totalSurfaceA());
+    }
+    
+}
---
+++ JavaProgram/OOPConcept/DatahidingEx.java
@@ -0,0 +1,68 @@
+package JavaProgram.OOPConcept;
+
+
+class Rectangle1{
+
+    private int length;
+    private int breadth;
+
+    // constructors Introduced...
+    public Rectangle1()
+    {
+        length = 1;
+        breadth = 1;
+    }
+
+    public Rectangle1(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+
+    int getLength(){
+        return length;
+    }
+
+    void setLength(int l){
+        if(l>0){
+            length = l;
+        }
+        else{
+            length = 0;
+        }
+    }
+    
+    int getBreadth(){
+        return breadth;
+    }
+
+    void setBreadth(int b){
+        if(b>0)
+        {
+            breadth = b;
+        }
+        else{
+            length = 0;
+        }
+    }
+    public int area(){
+        return length *breadth;
+    }
+
+    public int breadth(){
+        return 2*(length+breadth);
+    }
+}
+
+
+public class DatahidingEx {
+
+    public static void main(String[] args) {
+        Rectangle1 r1 = new Rectangle1(10,20);
+        // r1.setLength(-10);
+        // r1.setBreadth(50);
+
+        System.out.println(r1.area());
+    }
+    
+}
