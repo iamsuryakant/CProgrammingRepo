1fad3d4d9b7b95e5c20e48567077aa9a45db40d6
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-07-04 19:19:39 +0530

    update programs

---
+++ JavaProgram/OOPConcept/checkSum.java
@@ -0,0 +1,30 @@
+package JavaProgram.OOPConcept;
+
+import java.util.*;
+
+public class checkSum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-->0)
+        {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+
+            if((a+b == c)||(b+c == a) ||(a+c == b))
+            {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+        t--;
+        in.close();
+
+    }
+
+}
+
