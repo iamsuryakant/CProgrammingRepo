98e7a50d15886df2bca53d4d8d138827a8ba4241
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-07-12 21:45:33 +0530

    added codes

--- JavaProgram/OOPConcept/stringPali.java
+++ JavaProgram/OOPConcept/stringPali.java
@@ -4,35 +4,56 @@ package JavaProgram.OOPConcept;
 
 class stringPali{
     
+
+    // static boolean pali(String str, int l, int r) {
+
+    //     if(l<r)
+    //     {
+    //         if(str.charAt(l) != str.charAt(r))
+    //             return false;
+    //         pali(str, l+1, r-1);
+    //     }
+    //     return true;
+    // }
+
     public static void main(String[] args) {
         
-        String s1 = new String("GFG");
-        // String s2 = s1;
+        String s1 = "Arin";
+        String s2 = "Arin";
 
-        int n = s1.length();
-        int start = 0 ;
-        int end = n-1;
-        for(int i = 0; i < n; i++) {
+        System.out.println(s1.equals(s2));
+
+                
+        
+        // String s1 = "APPA";
+        // // String s2 = s1;
+
+        // int n = s1.length();
+
+        // System.out.println(pali(s1, 0, n-1));
+        // int start = 0 ;
+        // int end = n-1;
+        // for(int i = 0; i < n; i++) {
             
-            while(start < end)
-            {
-                if(s1.charAt(start) != s1.charAt(end))
-                {
-                    break;
-                }
-                start++;
-                end--;   
-            }
+        //     while(start < end)
+        //     {
+        //         if(s1.charAt(start) != s1.charAt(end))
+        //         {
+        //             break;
+        //         }
+        //         start++;
+        //         end--;   
+        //     }
 
-        }
+        // }
 
-        if(start == end)
-        {
-            System.out.println("String is palindrome");
-        }
-        else
-        {
-            System.out.println("String is not palindrome");
-        }
+        // if(start == end)
+        // {
+        //     System.out.println("String is palindrome");
+        // }
+        // else
+        // {
+        //     System.out.println("String is not palindrome");
+        // }
     }
 }
