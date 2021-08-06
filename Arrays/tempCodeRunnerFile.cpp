cedc5b0b7dfc25cb79540a35797da799feabed0c
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-08-06 17:15:49 +0530

    added codes

--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,14 +1,2 @@
-int main(){
-    int n, pos;
-    cin>>n;
 
-    int arr[n];
-
-    for(int i = 0; i < n; i++)
-    {
-        cin>>pos>>arr[i];
-    }
-
-    ArrInsertatGivenPos(pos, arr, n);
-
-}
+           // arr[i-2] = -1;
--- Codechef/b.cpp
+++ Codechef/b.cpp
@@ -1,4 +1,4 @@
-// Codeforces 
+//problem diff
 
 #include<bits/stdc++.h>
 //#include<ext/pb_ds/assoc_container.hpp>
@@ -47,96 +47,42 @@ void file_i_o()
     #endif
 }
 
-// int firstMissing(vector<int> &arr, int start, int end){
-
-//     if(start > end)
-//         return end+1;
-    
-//     if(start != arr[start])
-//         return start;
-
-//     int mid = (start + end)/2;
-
-
-//     if(arr[mid] == mid)
-//         return firstMissing(arr, mid+1, end);
-
-    
-//     return firstMissing(arr, start, mid);
-// }
-
-
-
 int main(int argc, char const *argv[]) {
     file_i_o();
 
     int t; cin>>t;
 
-    while(t--){
+    while(t--) {
 
-        // int n,m;
-        // cin>>n>>m;
-
-        // vector<int> arr;
-        // for(int i = 0; i<=m ; i++)
-        // {
-        //     int inp = n^i;
-
-        //     arr.pb(inp);
-        // }
-
-        // sort(arr.begin(), arr.end());
-
-        // cout<<firstMissing(arr, 0, m-1)<<"\n";
-
-    int n, m; cin >> n >> m;
-    ++m;
-    int ans = 0;
-    for (int k = 30; k >= 0 and n < m; k--) {
-      if ((n >> k & 1) == (m >> k & 1)) continue;
-      if (m >> k & 1) ans |= 1 << k, n |= 1 << k;
-    }
-    cout << ans << '\n';
-
-    //    if(arr[0] != 0){
-    //         cout<<0<<"\n";
-    //         break;
-    //    }
+        int n = 4, count = 0;
+        int arr[n];
         
-    //     else{
-    //         for(int i = 1; i<=m ; i++)
-    //         {
-    //             //arr[i-2] = -1;
-    //             //int temp = arr[0];
+        cin>>arr[0]>>arr[1]>>arr[2]>>arr[3];
+        sort(arr,arr+n);
 
-    //             if((arr[i] - arr[i-1]) == 1){
-    //                 continue;
-    //             }
-
-    //             else{
-
-    //                 // if(arr[i] - arr[i-1] == -1)
-    //                 //     cout<<"0"<<endl;
-                    
-    //                 if(arr[i] - arr[i-1] != 1){
-    //                     cout<<arr[i-1]+1<<endl;
-    //                     break;
-    //                 }
-
-
-    //             }
-    //         }
-    //     }
-
-        // for(int i = 0; i<=m; i++)
-        // {
-        //     cout<<arr[i]<<" ";
-        // }
-
-
-
-
+        if(arr[0]!=arr[1] && arr[1]!=arr[2] && arr[2]!=arr[3])
+        {
+            count += 2;
+            cout<<count<<"\n";
+        }
+        else if(arr[0]!=arr[1] && arr[1]!= arr[2] && arr[2]==arr[3]
+                || arr[0]!=arr[1] && arr[1]==arr[2] && arr[2]!=arr[3]
+                || arr[0]==arr[1] && arr[1]!=arr[2] && arr[2]!=arr[3]
+                || arr[0]==arr[1] && arr[1]!=arr[2] && arr[2]==arr[3]){
+            count += 2;
+            cout<<count<<"\n";
+        }
+        else if(arr[0]!=arr[1] && arr[1]==arr[2] && 
+                arr[2]==arr[3]|| arr[0]==arr[1] && arr[1]==arr[2] && arr[2]!=arr[3]){
+            count += 1;
+            cout<<count<<"\n";
+        }
+        else{
+            cout<<count<<"\n";
+        }
 
     }
     return 0;
 }
+
+
