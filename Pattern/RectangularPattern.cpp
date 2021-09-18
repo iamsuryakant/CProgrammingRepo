aef06fd163097ff08372ff10a6634d80f92c085e
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-09-18 08:28:39 +0530

    added codes

--- Pattern/RectangularPattern.cpp
+++ Pattern/RectangularPattern.cpp
@@ -10,6 +10,15 @@ int main(){
 
     std::cin>>c1>>c2>>n;
 
-    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            std::cout<<(std::max(abs(c1-i), abs(c2-j)))<<" ";
+        }
+        std::cout<<"\n";
+    }
+
+    return 0;
 
 }
