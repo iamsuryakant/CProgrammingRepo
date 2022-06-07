cecfee50fada06084154b666c0b556c4f4f543bd
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-06-08 00:48:16 +0530

    added codes

--- Recursion/arrdec.cpp
+++ Recursion/arrdec.cpp
@@ -38,7 +38,90 @@ void file_i_o()
 #endif
 }
 
+bool isEqual(int arr1[], int n, int arr2[]) {
+
+	int b1 = arr1[0];
+	int b2 = arr2[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		b1 ^= arr1[i];
+	}
+
+	for (int i = 1; i < n; i++)
+	{
+		b2 ^= arr1[i];
+	}
+
+	int all = b1 ^ b2;
+
+	if (all == 0)
+	{
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// void oper(int arr1[], int n, int arr2[]) {
+
+// 	int oper = 0;
+// 	if (isEqual(arr1, n, arr2)) {
+// 		cout << "YES" << endl;
+// 	} else {
+// 		for (int i = 0; i < n; i++)
+// 		{
+// 			arr1[i] = arr1[i] - 1;
+// 		}
+// 		oper++;
+// 		oper(arr1, n, arr2);
+// 	}
+
+// 	if (oper) {
+// 		cout << "YES" << endl;
+// 	} else {
+// 		cout << "NO" << endl;
+// 	}
+// }
+
+
 void solve() {
+	int t; cin >> t;
+
+	while (t--) {
+		int n; cin >> n;
+
+		int arr1[n], arr2[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr1[i];
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr2[i];
+		}
+
+		sort(arr1, arr1 + n);
+		sort(arr2, arr2 + n);
+
+		int oper = 0;
+
+		for (int i = 0; i < n; i++) {
+			oper += abs(arr1[i] - arr2[i]);
+		}
+
+		cout << oper << endl;
+
+		// if (oper) {
+		// 	cout << "yES" << endl;
+		// } else {
+		// 	cout << "No" << endl;
+		// }
+
+
+	}
 
 }
 
--- Recursion/pedestrl.cpp
+++ Recursion/pedestrl.cpp
@@ -39,8 +39,56 @@ void file_i_o()
 }
 
 void solve() {
+	ll t; cin >> t;
+
+	while (t--) {
+		ll n; cin >> n;
+
+		//vi ans;
+
+		ll a = 0, b = 0, c = 0, on = 0;
+		on = n;
+
+		c = (n - 3) / 3;
+		n = n - c;
+		if (n & 1) {
+			a = (n + 1) / 2;
+			b = (n - 1) / 2;
+		} else {
+			a = (n + 2) / 2;
+			b = on - (a + c);
+		}
+
+		cout << b << " " << a << " " << c << endl;
+
+
+
+	}
+
+
+	// for (int i = 0; i < ans.size(); i++)
+	// {
+	// 	if (ans[i] > ans[i + 1]) {
+	// 		swap(ans[i], ans[i + 1]);
+	// 		break;
+	// 	}
+	// }
+
+	// ans.push_back(b);
+	// ans.push_back(a);
+	// ans.push_back(c);
+
+
+	// for (int i = 0; i < ans.size(); i++)
+	// {
+	// 	cout << ans[i] << " ";
+	// }
+	// cout << endl;
+
+
 
 }
+
 
 
 int main() {
@@ -48,3 +96,9 @@ int main() {
 	solve();
 	return 0;
 }
+
+// 6
+// 10
+// 100000
+// 7
+// 8
